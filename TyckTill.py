3ec37ff22af6007eea6c3919b497cbea46da65f9
Lisas-hub <kajsa.henrietta@gmail.com>
2025-09-15 14:21:50 +0200

    Upload to GitHub

--- TyckTill.py
+++ TyckTill.py
@@ -59,8 +59,6 @@ print("\n--- Overview of Fritext ---")
 tycktill_df['comment_length'] = tycktill_df['Fritext'].astype(str).apply(len)
 tycktill_df['word_count'] = tycktill_df['Fritext'].astype(str).apply(lambda x: len(x.split()))
 
-tycktill_df['comment_length'] = tycktill_df['Fritext'].astype(str).apply(len)
-
 summary = tycktill_df.groupby('Kategori').agg(
     entry_count=('Kategori', 'count'),
     avg_comment_length=('comment_length', 'mean')
@@ -98,6 +96,68 @@ for t in kategorier:
     HeatMap(heat_data, radius=10, blur=5, min_opacity=0.3).add_to(m)
 
     m.save(f"data/heatmap_type_{t}.html")
+
+
+# ===========================
+# ===== join with parks =====
+
+parks = gpd.read_file("data/VARIABLES_NEW.gpkg", layer="VARIABLES_base")
+
+tycktill_pts = gpd.GeoDataFrame(
+    tycktill_df, geometry=gpd.points_from_xy(
+        tycktill_df['Koordinater_x'],
+        tycktill_df['Koordinater_Y']
+    ),
+    crs=4326)
+
+tycktill_pts.to_file("data/tycktill.gpkg", layer="tycktill_pts", driver="GPKG", mode="w")
+
+tycktill_pts_copy = tycktill_pts.copy()
+
+tycktill_pts_copy = tycktill_pts_copy.to_crs(parks.crs)
+
+parks_x_tycktill = gpd.sjoin(tycktill_pts_copy, parks, how="left", predicate="within")
+parks_x_tycktill = parks_x_tycktill.drop(columns='index_right', errors='ignore')
+
+parks_x_tycktill.to_file("data/tycktill.gpkg", layer="parks_x_tycktill", driver="GPKG", mode="w")
+
+
+# ===========================
+# ===== clip with parks =====
+
+tycktill_pts_in_parks = gpd.sjoin(tycktill_pts_copy, parks[['group', 'geometry']], how="inner", predicate="within")
+tycktill_pts_in_parks = tycktill_pts_in_parks.drop(columns='index_right', errors='ignore')
+
+tycktill_pts_in_parks.to_file("data/tycktill.gpkg", layer="tycktill_pts_in_parks", driver="GPKG", mode="w")
+
+tycktill_pts_in_parks['comment_length'] = tycktill_pts_in_parks['Fritext'].astype(str).apply(len)
+tycktill_pts_in_parks['word_count'] = tycktill_pts_in_parks['Fritext'].astype(str).apply(lambda x: len(x.split()))
+
+summary = tycktill_pts_in_parks.groupby('Kategori').agg(
+    entry_count=('Kategori', 'count'),
+    avg_comment_length=('comment_length', 'mean')
+).reset_index()
+
+print(summary)
+
+print("\n--- Overview of Fritext in parks (most common words) ---")
+all_words = ' '.join(tycktill_pts_in_parks['Fritext'].dropna().astype(str)).lower().split()
+word_counts = Counter(all_words)
+print(word_counts.most_common(50)) # 'klotter', 'skräp' och 'papperskorg' finns i top50 (övriga är 'på', 'och', 'är' osv)
+
+# =====================================
+# === update parks w tycktill stats ===
+
+summary = tycktill_pts_in_parks.groupby('group').agg(
+    num_points=('geometry', 'count'),
+    unique_comments=('Fritext', 'nunique'),
+).reset_index()
+
+summary['pts_per_hectare'] = summary['num_points'] / (parks.set_index('group').loc[summary['group'], 'park_area'] / 10000)
+
+parks = parks.merge(summary, on='group', how='left')
+parks.to_file("data/tycktill.gpkg", layer="tycktill_stats_per_park", driver="GPKG", mode="w")
+
 
 
 # ===========================
