ec632a8dcfbe364b223f19e4fad1bb468b2b1693
Lisas-hub <kajsa.henrietta@gmail.com>
2025-10-03 15:57:31 +0200

    .

--- TyckTill_Processing2A.py
+++ TyckTill_Processing2A.py
@@ -48,7 +48,7 @@ tycktill_df_geo["in_park"] = tycktill_df_geo.index.isin(subset_within_parks.inde
 # subset by parks and a limited number of rows
 
 # subset tycktill dataset to begin with before committing to processing all 300000+ rows
-kategori_filter = "Klagomål"
+kategori_filter = "Felanmälan"
 #in_park_filter = True
 # ^^RERUN? Update here!^^
 
@@ -90,35 +90,35 @@ subset_tycktill_df["lemmas"] = lemmatized_rows
 # ===============
 # Topic Modelling    *** move to after sentiments and run topic model on all rows at once? ***
 
-from bertopic import BERTopic
-from sentence_transformers import SentenceTransformer
+#from bertopic import BERTopic
+#from sentence_transformers import SentenceTransformer
 
-texts = subset_tycktill_df['lemmas'].apply(lambda words: ' '.join(words)).tolist()
+#texts = subset_tycktill_df['lemmas'].apply(lambda words: ' '.join(words)).tolist()
 
-embedding_model = SentenceTransformer("paraphrase-multilingual-MiniLM-L12-v2")
+#embedding_model = SentenceTransformer("paraphrase-multilingual-MiniLM-L12-v2")
 
 # create topic model
-topic_model = BERTopic(
-    embedding_model=embedding_model,
-    language="swedish",
-    min_topic_size=2, # probably adjust this one when using a bigger subset
-    verbose=True
-)
+#topic_model = BERTopic(
+#    embedding_model=embedding_model,
+#    language="swedish",
+#    min_topic_size=2, # probably adjust this one when using a bigger subset
+#    verbose=True
+#)
 
-topics, probs = topic_model.fit_transform(texts)
-subset_tycktill_df['topic'] = topics
-subset_tycktill_df['topic_prob'] = probs
+#topics, probs = topic_model.fit_transform(texts)
+#subset_tycktill_df['topic'] = topics
+#subset_tycktill_df['topic_prob'] = probs
 
-topic_info = topic_model.get_topic_info()
-topic_words = topic_model.get_topic(0)
-print(topic_info)
-print(topic_words)
+#topic_info = topic_model.get_topic_info()
+#topic_words = topic_model.get_topic(0)
+#print(topic_info)
+#print(topic_words)
 
-def get_top_words(topic_num):
-    words = topic_model.get_topic(topic_num)
-    return ', '.join([word for word, _ in words]) if words else ''
+#def get_top_words(topic_num):
+#    words = topic_model.get_topic(topic_num)
+#    return ', '.join([word for word, _ in words]) if words else ''
 
-subset_tycktill_df['topic_keywords'] = subset_tycktill_df['topic'].apply(get_top_words)
+#subset_tycktill_df['topic_keywords'] = subset_tycktill_df['topic'].apply(get_top_words)
 
 # ====
 # Save
