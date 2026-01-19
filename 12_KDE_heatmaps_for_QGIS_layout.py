25095cdb8a13552a432fae607ffea1816784e6bc
Lisas-hub <kajsa.henrietta@gmail.com>
2026-01-19 16:27:45 +0100

    .

--- 12_KDE_heatmaps_for_QGIS_layout.py
+++ 12_KDE_heatmaps_for_QGIS_layout.py
@@ -23,6 +23,7 @@ radius_m = 250       # KDE bandwidth in meters
 pixel_size = 50      # pixel size in meters
 gaussian_sigma = 5   # only applied to Praise + Ideas
 nodata_val = -9999.0
+GLOBAL_KDE_MAX = {}
 
 categories = {
     "Praise": ["Beröm"],
@@ -61,7 +62,7 @@ if points.crs != boundary.crs:
 # ====================================
 # helper function to create KDE raster
 
-def create_kde_raster(points_gdf, boundary_gdf, radius_m, pixel_size, gaussian_sigma=None):
+def create_kde_raster(points_gdf, boundary_gdf, radius_m, pixel_size, category, gaussian_sigma=None, normalize=False):
 
     n_points = len(points_gdf)
 
@@ -82,10 +83,7 @@ def create_kde_raster(points_gdf, boundary_gdf, radius_m, pixel_size, gaussian_s
     kde = KernelDensity(bandwidth=radius_m, kernel='gaussian')
     kde.fit(coords)
 
-    #Z_kde = np.exp(kde.score_samples(grid_coords)).reshape(X.shape)
-    ###############
     # normalise
-    # KDE (integrates to 1)
     kde = KernelDensity(bandwidth=radius_m, kernel="gaussian")
     kde.fit(coords)
 
@@ -93,19 +91,39 @@ def create_kde_raster(points_gdf, boundary_gdf, radius_m, pixel_size, gaussian_s
 
     # Scale by number of points (absolute density)
     Z_raw = Z_raw * n_points
-    ###############
 
     # extra gaussian smoothing
     Z_gauss = None
     if gaussian_sigma is not None:
         Z_gauss = gaussian_filter(Z_raw, sigma=gaussian_sigma)
 
-    #Z_kde = Z_kde / Z_kde.max()
-    #if Z_gauss is not None:
-    #    Z_gauss = Z_gauss / Z_gauss.max()
+    # update global max (for first pass)
+    #global GLOBAL_KDE_MAX
+    #local_max = np.nanmax(Z_gauss if Z_gauss is not None else Z_raw)
+    #GLOBAL_KDE_MAX = max(GLOBAL_KDE_MAX, local_max)
+
+    global GLOBAL_KDE_MAX
+
+    local_max = np.nanmax(Z_gauss if Z_gauss is not None else Z_raw)
+
+    if category not in GLOBAL_KDE_MAX:
+        GLOBAL_KDE_MAX[category] = local_max
+    else:
+        GLOBAL_KDE_MAX[category] = max(GLOBAL_KDE_MAX[category], local_max)
+
+
 
     # rasterio transform
     transform = from_origin(minx, maxy, pixel_size, pixel_size)
+
+    # normalise to get 0-1
+    if normalize:
+        if category not in GLOBAL_KDE_MAX or GLOBAL_KDE_MAX[category] <= 0:
+            raise ValueError(f"No global max for category {category}")
+
+        Z_raw = Z_raw / GLOBAL_KDE_MAX[category]
+        if Z_gauss is not None:
+            Z_gauss = Z_gauss / GLOBAL_KDE_MAX[category]
 
     return Z_raw, Z_gauss, transform, Z_raw.shape
 
@@ -149,8 +167,16 @@ def save_raster(Z, transform, shape, out_path, boundary_gdf, nodata_val):
     with rasterio.open(out_path, "w", **out_meta_clipped) as dst:
         dst.write(out_image)
 
-def run_kde(points, time_groups, time_col, label_name):
+def run_kde(points, time_groups, time_col, label_name, normalize=False):
     summary = []
+
+    # global GLOBAL_KDE_MAX
+    # if not normalize:
+    #     GLOBAL_KDE_MAX = 0.0
+
+    global GLOBAL_KDE_MAX
+    if not normalize:
+        GLOBAL_KDE_MAX = {}
 
     total_jobs = len(categories) * len(time_groups)
     job_counter = 0
@@ -183,104 +209,63 @@ def run_kde(points, time_groups, time_col, label_name):
                 boundary,
                 radius_m,
                 pixel_size,
-                gaussian_sigma
+                category=cat_name,
+                gaussian_sigma=gaussian_sigma,
+                normalize=normalize
             )
 
             if Z_raw is None:
                 continue
 
-            # RAW
-            save_raster(
-                Z_raw,
-                transform,
-                shape,
-                os.path.join(output_folder, f"KDE_{cat_name}_{group_name}_RAW.tif"),
-                boundary,
-                nodata_val
-            )
-
-            # GAUSSIAN
-            if Z_gauss is not None:
+            if normalize:
+                # RAW
                 save_raster(
-                    Z_gauss,
+                    Z_raw,
                     transform,
                     shape,
-                    os.path.join(output_folder, f"KDE_{cat_name}_{group_name}_GAUSS.tif"),
+                    os.path.join(output_folder, f"KDE_{cat_name}_{group_name}_RAW_NORM.tif"),
                     boundary,
                     nodata_val
                 )
 
+                if Z_gauss is not None:
+                    save_raster(
+                        Z_gauss,
+                        transform,
+                        shape,
+                        os.path.join(output_folder, f"KDE_{cat_name}_{group_name}_GAUSS_NORM.tif"),
+                        boundary,
+                        nodata_val
+                    )
+
     return pd.DataFrame(summary)
 
 
-# ===================================
-# loop through categories and seasons
+# seasons
+#season_summary = run_kde(points, seasons, "month", "Season", normalize=True)
 
-# for cat_name, cat_values in categories.items():
-#     for season_name, month_values in seasons.items():
-#
-#         # filter points
-#         subset = points[
-#             points["Kategori"].isin(cat_values) &
-#             points["month"].isin(month_values)
-#         ]
-#
-#         point_count = len(subset)
-#         kde_summary.append({
-#             "Category": cat_name,
-#             "Season": season_name,
-#             "Points": point_count
-#         })
-#
-#         Z_raw, Z_smooth, transform, shape = create_kde_raster(
-#             subset,
-#             boundary,
-#             radius_m,
-#             pixel_size,
-#             gaussian_sigma=gaussian_sigma
-#         )
-#
-#         if Z_raw is None:
-#             print(f"No points for {cat_name} – {season_name}, skipping.")
-#             continue
-#
-#         # RAW KDE
-#         raw_path = os.path.join(
-#             output_folder,
-#             f"KDE_{cat_name}_{season_name}_RAW.tif"
-#         )
-#         save_raster(Z_raw, transform, shape, raw_path, boundary, nodata_val)
-#
-#         # GAUSSIAN-SMOOTHED KDE
-#         if Z_smooth is not None:
-#             gauss_path = os.path.join(
-#                 output_folder,
-#                 f"KDE_{cat_name}_{season_name}_GAUSS.tif"
-#             )
-#             save_raster(Z_smooth, transform, shape, gauss_path, boundary, nodata_val)
-#
-#         print(f"Saved {cat_name} – {season_name}")
+# weekday vs weekend
+#weekday_summary = run_kde(points, week_groups, "weekday", "WeekType", normalize=True)
 
-#summary_df = pd.DataFrame(kde_summary)
+# time of day
+#hour_summary = run_kde(points, hour_groups, "hour", "TimeOfDay", normalize=True)
 
-# Seasons
+
+# ===== SEASONS =====
 season_summary = run_kde(points, seasons, "month", "Season")
+print(f"Season KDE max: {GLOBAL_KDE_MAX}")
+run_kde(points, seasons, "month", "Season", normalize=True)
 
-# Weekday / Weekend
+# ===== WEEKDAY / WEEKEND =====
 weekday_summary = run_kde(points, week_groups, "weekday", "WeekType")
+print(f"WeekType KDE max: {GLOBAL_KDE_MAX}")
+run_kde(points, week_groups, "weekday", "WeekType", normalize=True)
 
-# Time of day
+# ===== TIME OF DAY =====
 hour_summary = run_kde(points, hour_groups, "hour", "TimeOfDay")
+print(f"TimeOfDay KDE max: {GLOBAL_KDE_MAX}")
+run_kde(points, hour_groups, "hour", "TimeOfDay", normalize=True)
 
-
-# summary_df.to_csv(os.path.join(output_folder, "kde_season_counts.csv"), index=False)
-# weekday_summary.to_csv(os.path.join(output_folder, "kde_weekday_counts.csv"), index=False)
-# hour_summary.to_csv(os.path.join(output_folder, "kde_hour_counts.csv"), index=False)
-#
-#
-#
-# print("\nKDE input point counts:")
-# print(summary_df.to_string(index=False))
 
 print("\nSeasonal KDE point counts:")
 print(season_summary.to_string(index=False))
@@ -292,6 +277,7 @@ print("\nTime-of-day KDE point counts:")
 print(hour_summary.to_string(index=False))
 
 
+# WITH GLOBAL MAX ACROSS ALL COMBINATIONS
 # Seasonal KDE point counts:
 #         Category Season  Points
 #           Praise Winter      95
