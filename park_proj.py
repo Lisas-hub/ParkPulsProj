2f552ee6f8f957fdfd4a1c4cacdc97cb00499a18
Lisas-hub <kajsa.henrietta@gmail.com>
2026-04-21 15:12:01 +0200

    .

--- park_proj.py
+++ park_proj.py
@@ -6,22 +6,22 @@
 import streamlit as st
 
 home_page = st.Page("home_page.py", title="Home Page")
-page_1 = st.Page("archive/page_1.py", title="Page 1") # mapping demo pydeck - clickable layers
-page_2 = st.Page("archive/page_2.py", title="Page 2") # mapping demo leafmap - clustered points
-page_3 = st.Page("archive/page_3.py", title="Page 3") # mapping demo leafmap - topographic map
-page_4 = st.Page("archive/page_4.py", title="Page 4") # busshållsplatser test
-page_5 = st.Page("archive/page_5.py", title="Page 5") # Google Sheets API test
-page_6 = st.Page("archive/page_6.py", title="Page 6") # Excel test
-page_7 = st.Page("archive/page_7.py", title="Page 7") # test folium map w sociotop map and attached attributes
-page_8 = st.Page("archive/page_8.py", title="Page 8") # leafmap splitmap test
-page_9 = st.Page("archive/page_9.py", title="Page 9") # pydeck test
-page_10 = st.Page("archive/page_10.py", title="Page 10") # OG bus stops test copy
-UGS = st.Page("archive/UGS.py", title="Urban Green Spaces")
-transport = st.Page("archive/Transport.py", title="Transportation")
-biotope = st.Page("archive/Biotope.py", title="Biotopes **slow to load**")
-noise = st.Page("archive/Noise_Pollution.py", title="Noise Pollution **slow to load**")
-socio_economic = st.Page("archive/Socio_economic.py", title="Socio economic")
-socio_economic2 = st.Page("archive/Socio_economic_2.py", title="Socio economic 2")
+#page_1 = st.Page("archive/page_1.py", title="Page 1") # mapping demo pydeck - clickable layers
+#page_2 = st.Page("archive/page_2.py", title="Page 2") # mapping demo leafmap - clustered points
+#page_3 = st.Page("archive/page_3.py", title="Page 3") # mapping demo leafmap - topographic map
+#page_4 = st.Page("archive/page_4.py", title="Page 4") # busshållsplatser test
+#page_5 = st.Page("archive/page_5.py", title="Page 5") # Google Sheets API test
+#page_6 = st.Page("archive/page_6.py", title="Page 6") # Excel test
+#page_7 = st.Page("archive/page_7.py", title="Page 7") # test folium map w sociotop map and attached attributes
+#page_8 = st.Page("archive/page_8.py", title="Page 8") # leafmap splitmap test
+#page_9 = st.Page("archive/page_9.py", title="Page 9") # pydeck test
+#page_10 = st.Page("archive/page_10.py", title="Page 10") # OG bus stops test copy
+#UGS = st.Page("archive/UGS.py", title="Urban Green Spaces")
+#transport = st.Page("archive/Transport.py", title="Transportation")
+#biotope = st.Page("archive/Biotope.py", title="Biotopes **slow to load**")
+#noise = st.Page("archive/Noise_Pollution.py", title="Noise Pollution **slow to load**")
+#socio_economic = st.Page("archive/Socio_economic.py", title="Socio economic")
+#socio_economic2 = st.Page("archive/Socio_economic_2.py", title="Socio economic 2")
 parks = st.Page("Parks.py", title="Parks")
 parks2 = st.Page("Parks2.py", title="Parks2")
 
